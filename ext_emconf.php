21f21818e58163e0cb4a59ce3e41b74ffa879a7f
Kathara <mgreber@gmx.ch>
2018-08-16 14:19:20 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '5.5.0-7.1.99',
-            'typo3' => '7.6.0-7.6.99'
+            'typo3' => '8.7'
         ],
         'conflicts' => [
             'kb_tv_clipboard' => '-0.1.0',
